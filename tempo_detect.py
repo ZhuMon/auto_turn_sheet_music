d8455e245ded184ed6a975d63b572ee4138e51cb
ZhuMon <richardlin0212@gmail.com>
2021-06-08 00:11:44 +0800

    Detect tempo & Add new music

---
+++ tempo_detect.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python3
+
+import sys
+from aubio import tempo, source
+
+
+def detect_tempo(file_name):
+    win_s = 512         # fft size
+    hop_s = win_s // 2    # hop size
+
+    samplerate = 0 
+
+    s = source(file_name, samplerate, hop_s)
+    samplerate = s.samplerate
+
+    o = tempo("default", buf_size = win_s, hop_size = hop_s, samplerate = samplerate)
+
+    # tempo detection delay, in samples
+    # default to 4 blocks delay to catch up with
+    delay = 4. * hop_s
+
+    # list of beats, in samples
+    beats = []
+
+    # total number of frames read
+    total_frames = 0
+    while True:
+        samples, read = s()
+        is_beat = o(samples)
+        if is_beat:
+            this_beat = int(total_frames - delay + is_beat[0] * hop_s)
+            # print("%f" % (this_beat / float(samplerate)))
+            beats.append(this_beat)
+        total_frames += read
+        if read < hop_s: break
+
+    bpms = []
+    for i in range(len(beats)-1):
+        bpms.append(60 / ((beats[i+1] - beats[i]) / float(samplerate)))
+
+    print(sum(bpms)/len(bpms))
+
+    # print(len(beats))
+
+def gpx2wav(file_name):
+    pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} <filename>")
+
+    detect_tempo(sys.argv[1])
